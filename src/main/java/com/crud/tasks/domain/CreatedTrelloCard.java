f615b0755397e76b4dcc2f342f6aaf8d8aa1b993
matimarkiewicz <mati.markiewicz@gmail.com>
2019-09-04 12:15:24 +0200

    modul_22_4

--- src/main/java/com/crud/tasks/domain/CreatedTrelloCard.java
+++ src/main/java/com/crud/tasks/domain/CreatedTrelloCard.java
@@ -21,6 +21,6 @@ public class CreatedTrelloCard {
     @JsonProperty("shortUrl")
     private String shortUrl;
 
-    @JsonProperty("badges")
-    private TrelloBadges badges;
+//    @JsonProperty("badges")
+//    private TrelloBadges badges;
 }
